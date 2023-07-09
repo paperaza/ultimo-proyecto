35d67d59ed3ead9de99af395f2ab802ededfbebb
paperaza <paperaza13@gimail.com>
2023-07-09 18:23:21 -0500

    Primer lanzamiento proyecto con autenticacion funcional.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider 
+      domain='dev-acwgp8dwoyrwudhk.us.auth0.com'
+      clientId='yF4q2E9dAkG0hXsT0pwZYqSWpqSXErDM' 
+      redirectUri={window.location.origin}//hasta que se haga lo anterior si puede ir a la App
+    >
     <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();

e1066113c165bdd5c423a08cc80580849c733192
paperaza <paperaza13@gimail.com>
2023-07-15 22:39:41 -0500

    Autenticacion funcional con redux

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import * as S from "./Log.styles";
+import { useDispatch } from 'react-redux';
+import  login  from '../actions/userActions';
 
 export const Login = () => {
-    const { loginWithRedirect } = useAuth0();
 
-    return <button onClick={() => loginWithRedirect()}>Igresar</button>;
+    const { loginWithRedirect, isAuthenticated, user, isLoading} = useAuth0();
+    const dispatch = useDispatch ();
+    const handleLogin = async () => {
+        try {
+            await loginWithRedirect();
+        } catch (error) {
+
+            console.log(error);
+            
+        }
+
+    };
+
+    useEffect (() => {
+        if (isAuthenticated && user) {
+            dispatch(login(user))
+        };
+    }, [isAuthenticated, user, dispatch]);
+
+    if (isLoading) {
+        return <div>Cargando...</div>
+    }
+
+    return  <S.ContainerButton>
+                <S.Button onClick={handleLogin}>Igresar</S.Button>
+            </S.ContainerButton>;
+            
 };
 
-export default Login
+export default Login;
+
+

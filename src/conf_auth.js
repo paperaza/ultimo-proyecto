654c71f24a72c04fe1725d05641b7c68130070d3
paperaza <paperaza13@gimail.com>
2023-07-16 18:15:04 -0500

    Variables de entorno funcionales

---
+++ src/conf_auth.js
@@ -0,0 +1,7 @@
+const conf_auth = {
+    domain: 'dev-acwgp8dwoyrwudhk.us.auth0.com',
+    clientId: 'yF4q2E9dAkG0hXsT0pwZYqSWpqSXErDM',
+    redirectUri: window.location.origin,
+}
+
+export default conf_auth;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
 import { Provider } from 'react-redux';
 import store from './store/store';
+import conf_auth from './conf_auth';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Auth0Provider
-        domain='dev-acwgp8dwoyrwudhk.us.auth0.com'
-        clientId='yF4q2E9dAkG0hXsT0pwZYqSWpqSXErDM'
-        redirectUri={window.location.origin}//hasta que se haga lo anterior si puede ir a la App
-      >
+      <Auth0Provider {...conf_auth}>
         <App />
       </Auth0Provider>
     </Provider>
@@ -22,4 +18,4 @@ root.render(
 );
 
 
-reportWebVitals();
+

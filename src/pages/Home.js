396cd2c84c75920bc18568080ab3ea47e60c9fce
paperaza <paperaza13@gimail.com>
2023-07-16 20:16:19 -0500

    Estilos en tajetas

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,39 +2,47 @@ import { useAuth0 } from '@auth0/auth0-react'
 import React, { Fragment } from 'react'
 import Axios from 'axios'
 import { useEffect, useState } from 'react';
+import * as S from './Home.styles';
 
 const Home = () => {
-    const { user } = useAuth0();
-    const [celebrity, setCelebrity] = useState([]);
+  const { user } = useAuth0();
+  const [celebrity, setCelebrity] = useState([]);
 
-    const key = process.env.REACT_APP_KEY;
-    const headers = {
-      'X-Api-Key': key
-    }
-    
-    const nombre ='Michael Jordan'; 
-    const url =process.env.REACT_APP_URL+nombre;
-    useEffect(()=>{ 
-      Axios.get(url, {headers})
-      .then(res=>{console.log(res.data) 
-       setCelebrity(res.data)
-       console.log(celebrity)})
-      .catch(error=>{console.log(error)})
-    },[])
+  const key = process.env.REACT_APP_KEY;
+  const headers = {
+    'X-Api-Key': key
+  }
 
-    return(
+  const nombre = 'Michael Jordan';
+  const url = process.env.REACT_APP_URL + nombre;
+  useEffect(() => {
+    Axios.get(url, { headers })
+      .then(res => {
+        console.log(res.data)
+        setCelebrity(res.data)
+        console.log(celebrity)
+      })
+      .catch(error => { console.log(error) })
+  }, [])
+
+  return (
+    <div>
+      <h2>{user.name}</h2>
       <div>
-        <h2>{user.name}</h2>
-        <div>
-        {celebrity.map(cel=>{return(
-          <Fragment>
-          <h4>{cel.name}</h4>
-          <h3>{cel.age}</h3>
-          </Fragment>
-        )})}
-        </div>  
+        {celebrity.map(cel => {
+          return (
+            <S.ContentCharacter>
+              <Fragment>
+                <h4>{cel.name}</h4>
+                <h3>{cel.age}</h3>
+              </Fragment>
+            </S.ContentCharacter>
+          )
+        })}
       </div>
-    )
+    </div>
+  )
 }
+
 
 export default Home;
